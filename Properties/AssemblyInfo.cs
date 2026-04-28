574a83e869843fc32bcb4cd87f57f1e3f7fa9679
V@no <git@vano.org>
2026-04-27 21:55:53 -0400

    ﻿26.04.27.215539: build: sync version metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Rhino.PlugIns;
 [assembly: AssemblyProduct("vTools")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2607512e-a1fc-4cf9-9329-a293431437a0")]
-[assembly: AssemblyVersion("26.4.27.2137")]
-[assembly: AssemblyFileVersion("26.4.27.2137")]
-[assembly: AssemblyInformationalVersion("26.04.27.213705")]
+[assembly: AssemblyVersion("26.4.27.2155")]
+[assembly: AssemblyFileVersion("26.4.27.2155")]
+[assembly: AssemblyInformationalVersion("26.04.27.215539")]
 
 [assembly: PlugInDescription(DescriptionType.Address, "")]
 [assembly: PlugInDescription(DescriptionType.Country, "")]

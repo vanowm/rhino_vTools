4d8ea428199f6ac306c423d678701dc371253aaf
V@no <git@vano.org>
2026-05-14 10:07:15 -0400

    26.5.14.1006: vDiamonds: version bump

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Rhino.PlugIns;
 [assembly: AssemblyProduct("vTools")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2607512e-a1fc-4cf9-9329-a293431437a0")]
-[assembly: AssemblyVersion("26.5.14.948")]
-[assembly: AssemblyFileVersion("26.5.14.948")]
-[assembly: AssemblyInformationalVersion("26.5.14.948")]
+[assembly: AssemblyVersion("26.5.14.1006")]
+[assembly: AssemblyFileVersion("26.5.14.1006")]
+[assembly: AssemblyInformationalVersion("26.5.14.1006")]
 
 [assembly: PlugInDescription(DescriptionType.Address, "")]
 [assembly: PlugInDescription(DescriptionType.Country, "")]

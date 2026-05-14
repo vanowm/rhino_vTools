90e48b225f0fc171bd098f0370fc5663503f3a8d
V@no <git@vano.org>
2026-05-14 10:38:28 -0400

    26.5.14.1038: vDiamonds: bbox size to history, BySize centers pattern, count independent of countW/H

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Rhino.PlugIns;
 [assembly: AssemblyProduct("vTools")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2607512e-a1fc-4cf9-9329-a293431437a0")]
-[assembly: AssemblyVersion("26.5.14.1026")]
-[assembly: AssemblyFileVersion("26.5.14.1026")]
-[assembly: AssemblyInformationalVersion("26.5.14.1026")]
+[assembly: AssemblyVersion("26.5.14.1038")]
+[assembly: AssemblyFileVersion("26.5.14.1038")]
+[assembly: AssemblyInformationalVersion("26.5.14.1038")]
 
 [assembly: PlugInDescription(DescriptionType.Address, "")]
 [assembly: PlugInDescription(DescriptionType.Country, "")]
